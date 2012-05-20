95ef5315e608f62eb2f7ea474ee249775831e40a
Robert DeNicola <thedizzle23@gmail.com>
2012-05-20 17:13:27 -0400

    mssage

---
+++ TestsBetaAppFinals/TestsBetaAppFinalsTests/TestsBetaAppFinalsTests.h
@@ -0,0 +1,13 @@
+//
+//  TestsBetaAppFinalsTests.h
+//  TestsBetaAppFinalsTests
+//
+//  Created by Robert DeNicola on 5/20/12.
+//  Copyright (c) 2012 Developmental Applications. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface TestsBetaAppFinalsTests : SenTestCase
+
+@end
---
+++ TestsBetaAppFinals/TestsBetaAppFinalsTests/TestsBetaAppFinalsTests.m
@@ -0,0 +1,32 @@
+//
+//  TestsBetaAppFinalsTests.m
+//  TestsBetaAppFinalsTests
+//
+//  Created by Robert DeNicola on 5/20/12.
+//  Copyright (c) 2012 Developmental Applications. All rights reserved.
+//
+
+#import "TestsBetaAppFinalsTests.h"
+
+@implementation TestsBetaAppFinalsTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in TestsBetaAppFinalsTests");
+}
+
+@end
