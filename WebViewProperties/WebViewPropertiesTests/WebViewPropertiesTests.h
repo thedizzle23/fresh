d5dbde93597d0ca01223bd55496e285e0a8bbfcb
Robert DeNicola <thedizzle23@gmail.com>
2012-05-20 19:13:49 -0400

    plist files

---
+++ WebViewProperties/WebViewPropertiesTests/WebViewPropertiesTests.h
@@ -0,0 +1,13 @@
+//
+//  WebViewPropertiesTests.h
+//  WebViewPropertiesTests
+//
+//  Created by Robert DeNicola on 5/20/12.
+//  Copyright (c) 2012 Developmental Applications. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface WebViewPropertiesTests : SenTestCase
+
+@end
---
+++ WebViewProperties/WebViewPropertiesTests/WebViewPropertiesTests.m
@@ -0,0 +1,32 @@
+//
+//  WebViewPropertiesTests.m
+//  WebViewPropertiesTests
+//
+//  Created by Robert DeNicola on 5/20/12.
+//  Copyright (c) 2012 Developmental Applications. All rights reserved.
+//
+
+#import "WebViewPropertiesTests.h"
+
+@implementation WebViewPropertiesTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in WebViewPropertiesTests");
+}
+
+@end
