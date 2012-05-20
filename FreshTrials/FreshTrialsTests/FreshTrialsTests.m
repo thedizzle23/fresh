0fd69a0e07413e1666fe7b01db2021e319129219
Robert DeNicola <thedizzle23@gmail.com>
2012-05-20 02:11:43 -0400

    coommit test main

---
+++ FreshTrials/FreshTrialsTests/FreshTrialsTests.m
@@ -0,0 +1,32 @@
+//
+//  FreshTrialsTests.m
+//  FreshTrialsTests
+//
+//  Created by Robert DeNicola on 5/20/12.
+//  Copyright (c) 2012 Developmental Applications. All rights reserved.
+//
+
+#import "FreshTrialsTests.h"
+
+@implementation FreshTrialsTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in FreshTrialsTests");
+}
+
+@end
